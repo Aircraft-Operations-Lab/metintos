4b63334fd080e783a9db1e2bd61b601160d2111a
Javier García-Heras Carretero <gcarrete@ing.uc3m.es>
2021-04-08 13:14:44 +0200

    Fixing classifiers to match with the ones in https://pypi.org/classifiers/

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ setup(
   zip_safe=False,
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Researchers',      # Define that your audience are developers
+    'Intended Audience :: Science/Research',      # Define that your audience are developers
     'Intended Audience :: Education',
-    'Intended Audience :: Science',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: LGPL License',   # Again, pick a license
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',   # Again, pick a license
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: Microsoft :: Windows',
     'Operating System :: Unix',
